1b705b70d8679c264c75e7952cb203922d60d86e
Artem Smirnov <artem2243083@ya.ru>
2022-04-20 19:29:12 +0300

    base select variant

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,89 @@
+import React, { FC, useCallback, useState } from 'react';
+import Input from '../Input';
+import Loader from '../Loader';
+import Option from './components/Option';
+
+import './Select.css';
+
+export interface SelectOption {
+  /**
+   * Unique id
+   */
+  key: string | number;
+
+  /**
+   * Label will be displayed to user
+   */
+  label: string;
+}
+
+interface Props {
+  /**
+   * Select options to display
+   */
+  options: SelectOption[];
+
+  /**
+   * Event will be fired when filter is changed
+   */
+  onChangeFilter?: (filter: string) => void;
+
+  /**
+   * Event will be fired when selected value will be changed
+   */
+  onChange?: (key: SelectOption['key']) => void;
+
+  /**
+   * Show error text below component
+   */
+  error?: string;
+
+  /**
+   * Indicates loading data
+   */
+  isLoading?: boolean;
+}
+
+const Select: FC<Props> = ({ options, onChangeFilter, error, isLoading, onChange }) => {
+  const [currentSearchValue, setSearchValue] = useState('');
+  const [isOpened, setIsOpened] = useState(false);
+  const hasResults = options.length > 0;
+  const onChangeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchValue(value);
+    onChangeFilter?.(value);
+  }, [onChangeFilter]);
+
+  const onClickInput = useCallback(() => {
+    setIsOpened(!isOpened)
+  }, [isOpened]);
+
+  const onClickSelectOption = useCallback((key: SelectOption['key']) => {
+    onChange?.(key);
+    setIsOpened(false);
+
+    // get original option label by clicked key
+    const originalOption = options.find((item) => item.key === key);
+    setSearchValue(originalOption?.label ?? '');
+  }, [onChange, options]);
+
+  const renderedLoading = isLoading ? (
+    <div className='select__loader'>
+      <Loader />
+    </div>
+  ) : undefined;
+
+  return (
+    <div className='select'>
+      <div className='select__input'>
+        <Input onChange={onChangeInput} endAdornment={renderedLoading} onClick={onClickInput} value={currentSearchValue ?? ''} />
+        {error && <p className='select__error'>{error}</p>}
+      </div>
+      {isOpened && <div className='select__dropdown'>
+        {hasResults && options.map((option) => <Option key={option.key} data={option} onClick={onClickSelectOption} highlightedText={currentSearchValue} />)}
+        {!hasResults && <span className='select__nothing'>Nothing to show :(</span>}
+      </div>}
+    </div>
+)}
+
+export default Select;
---
+++ src/components/Select/components/Option/Option.tsx
@@ -0,0 +1,55 @@
+import { FC, useCallback, useMemo } from 'react';
+import { SelectOption } from '../../Select';
+
+import './Option.css';
+
+interface Props {
+  /**
+   * SelectOption data
+   */
+  data: SelectOption;
+
+  /**
+   * Selected flag will add checkmark at the end
+   */
+  isSelected?: boolean;
+
+  /**
+   * Event will be fired on click event
+   */
+  onClick?: (key: SelectOption['key']) => void;
+
+  /**
+   * Text will be highlighted with yellow background
+   */
+  highlightedText?: string;
+}
+
+const Option: FC<Props> = ({ data, onClick, isSelected, highlightedText }) => {
+  
+  const { key, label } = data;
+  const onClickWrapper = useCallback(() => {
+    onClick?.(key);
+  }, [key, onClick]);
+
+  const highlightRegExp = new RegExp(`(${highlightedText})`, 'gi');
+  const textParts = label.split(highlightRegExp);
+
+  const content = useMemo(() => {
+    if (!highlightedText) {
+      return label
+    }
+
+    return textParts.map((part, index) => 
+      <span key={`${part}_${index}`} className={part.toLowerCase() === highlightedText.toLowerCase() ? 'option__highlight' : '' }>
+        {part}
+      </span>
+    );
+  }, [highlightedText, label, textParts]);
+
+  return (
+    <div className='option' onClick={onClickWrapper}>{content}</div>
+  )
+}
+
+export default Option;
