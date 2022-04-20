b8df592dc37979940e7bb9d6cf18b56b0e3aab25
Artem Smirnov <artem2243083@ya.ru>
2022-04-20 19:07:10 +0300

    base input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import './Input.css';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+
+  /**
+   * End adornment text or JSX
+   */
+  endAdornment?: string | JSX.Element;
+}
+
+const Input: FC<Props> = ({ endAdornment, ...props }) => (
+  <div className='input'>
+    <input className='input__field' {...props} />
+    {endAdornment && (
+      <span className='input__end-adornment'>
+        {endAdornment}
+      </span>
+    )}
+  </div>
+)
+
+export default Input;
